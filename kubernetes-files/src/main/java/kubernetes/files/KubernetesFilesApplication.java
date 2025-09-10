c3481a2b8eb6b5c8a026ca2d5ee89d531d1c0a68
amolsuryabhans <amolsuryabhans@apporchid.com>
2025-09-10 23:06:30 +0530

    Added kubernetes files into separate project

---
+++ kubernetes-files/src/main/java/kubernetes/files/KubernetesFilesApplication.java
@@ -0,0 +1,13 @@
+package kubernetes.files;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesFilesApplication.class, args);
+	}
+
+}
---
+++ kubernetes-files/src/test/java/kubernetes/files/KubernetesFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package kubernetes.files;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
